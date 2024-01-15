c6de3896c5d30af41e925c03343194edebe32da4
jays9 <jays9@LAPTOP-B0FIS9EP>
2024-01-15 14:11:32 +0530

    Action method name change

--- Form_Ajax_Jquery/Controllers/HomeController.cs
+++ Form_Ajax_Jquery/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Form_Ajax_Jquery.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult POST()
         {
             return View();
         }

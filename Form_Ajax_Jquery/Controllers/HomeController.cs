8ffd4ba09d8a6115e89dc4332cfcf6d812acb9ab
jays9 <jays9@LAPTOP-B0FIS9EP>
2024-01-15 12:52:09 +0530

    Add project files.

---
+++ Form_Ajax_Jquery/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Ajax_Jquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Form_Ajax_Jquery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Form_Ajax_Jquery.MvcApplication" Language="C#" %>
